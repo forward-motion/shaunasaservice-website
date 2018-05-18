1b99623b15fc80ba00427a944c43651148965f04
shaunpersad <shaunpersad@gmail.com>
2018-05-18 16:28:05 -0400

    added fb tags

--- src/components/_global/templates/DefaultTemplate.jsx
+++ src/components/_global/templates/DefaultTemplate.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import '../../../styles/_global/templates/DefaultTemplate.scss';
 
@@ -9,6 +10,34 @@ const DefaultTemplate = (props) => {
 
     return (
         <div className="default-template">
+
+            <Helmet
+                title="Shaun (as a service) - You think it, I build it."
+                meta={[
+                    { name: 'description', content: `Send me your product idea, and I'll build you a prototype in 30 days or less.` },
+                    { name: 'keywords', content: 'mvp, consulting, freelancer, developer' },
+                ]}
+            >
+
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6c63ff" />
+                <meta name="msapplication-TileColor" content="#4db6ac" />
+                <meta name="theme-color" content="#ffffff" />
+
+
+                <meta property="og:url" content="https://shaunasaservice.com" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Shaun (as a service) - You think it, I build it." />
+                <meta property="og:description" content={`Send me your product idea, and I'll build you a prototype in 30 days or less.`} />
+                {/*<meta property="og:image" content="/recv-icon.png" />*/}
+
+            </Helmet>
+
+
             <Header />
             {props.children()}
         </div>
