5aff0efd10453ef48f901b01b7976e8b901fd476
shaunpersad <shaunpersad@gmail.com>
2018-05-19 14:08:53 -0400

    testing lambda

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -4,7 +4,7 @@ exports.handler = function submissionCreated(event, context, callback) {
 
 
     console.log('hello?');
-    console.log('event', event.body);
+    console.log('event', Object.keys(event.body));
 
     callback(null, {
         statusCode: 200
