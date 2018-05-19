8499e14e02d76d27cfa0e52e986120a67d4c02f9
shaunpersad <shaunpersad@gmail.com>
2018-05-19 13:17:21 -0400

    testing lambda

---
+++ src/lambda/submission-created.js
@@ -0,0 +1,42 @@
+const http = require('https');
+
+exports.handler = function submissionCreated(event, context, callback) {
+
+    console.log('event', event);
+    console.log('context', context);
+    
+    let body = '';
+
+    const data = JSON.stringify({
+        email: event.body.email,
+        api_key: process.env.GATSBY_CONVERTKIT_API_KEY
+    });
+
+    const request = http.request({
+        host: 'api.convertkit.com',
+        path: `/courses/${process.env.GATSBY_CONVERTKIT_SEQUENCE_ID}/subscribe`,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Content-Length': Buffer.byteLength(data)
+        }
+    }, function response(res) {
+
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+            body+= chunk;
+        });
+
+        res.on('end', function() {
+
+            callback(null, {
+                statusCode: 200
+            });
+        });
+    });
+
+    request.write(data);
+    request.end();
+};
