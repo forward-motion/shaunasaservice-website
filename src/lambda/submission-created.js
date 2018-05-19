68a7e130885833b05b4b50ad3f8d8a72d46cd0c3
shaunpersad <shaunpersad@gmail.com>
2018-05-19 14:01:09 -0400

    testing lambda

--- src/lambda/submission-created.js
+++ src/lambda/submission-created.js
@@ -2,42 +2,45 @@ const http = require('https');
 
 exports.handler = function submissionCreated(event, context, callback) {
 
+
     console.log('hello?');
 
-    console.log('event', event);
-
-    let body = '';
-
-    const data = JSON.stringify({
-        email: event.body.email,
-        api_key: process.env.GATSBY_CONVERTKIT_API_KEY
+    callback(null, {
+        statusCode: 200
     });
 
-    const request = http.request({
-        host: 'api.convertkit.com',
-        path: `/courses/${process.env.GATSBY_CONVERTKIT_SEQUENCE_ID}/subscribe`,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'Content-Length': Buffer.byteLength(data)
-        }
-    }, function response(res) {
-
-        res.setEncoding('utf8');
-
-        res.on('data', function (chunk) {
-            console.log('Response: ' + chunk);
-            body+= chunk;
-        });
-
-        res.on('end', function() {
-
-            callback(null, {
-                statusCode: 200
-            });
-        });
-    });
-
-    request.write(data);
-    request.end();
+    // let body = '';
+    //
+    // const data = JSON.stringify({
+    //     email: event.body.email,
+    //     api_key: process.env.GATSBY_CONVERTKIT_API_KEY
+    // });
+    //
+    // const request = http.request({
+    //     host: 'api.convertkit.com',
+    //     path: `/courses/${process.env.GATSBY_CONVERTKIT_SEQUENCE_ID}/subscribe`,
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json; charset=utf-8',
+    //         'Content-Length': Buffer.byteLength(data)
+    //     }
+    // }, function response(res) {
+    //
+    //     res.setEncoding('utf8');
+    //
+    //     res.on('data', function (chunk) {
+    //         console.log('Response: ' + chunk);
+    //         body+= chunk;
+    //     });
+    //
+    //     res.on('end', function() {
+    //
+    //         callback(null, {
+    //             statusCode: 200
+    //         });
+    //     });
+    // });
+    //
+    // request.write(data);
+    // request.end();
 };
